2719124750ba4cba4536cc384eab03fe7be8e78b
johncreed <b02701216@ntu.edu.tw>
2019-08-27 11:17:56 +0800

    Update gen code

--- yh.data/filter.2.5/grid.sh
+++ yh.data/filter.2.5/grid.sh
@@ -7,7 +7,7 @@ tasks(){
         for l in 1 0.1 0.01 0.001 0.0001
         do
             mkdir -p logs.$l.k80
-            echo "./oc-train -l $l -w 0 -a 0 -k 80 -t 200 -c 5 -p $te $tr > logs.${l}.k80/${te}.${l}.k80.std"
+            echo "./train -l $l -w 0 -a 0 -k 80 -t 200 -c 5 -p $te $tr > logs.${l}.k80/${te}.${l}.k80.std"
         done
     done
 }
